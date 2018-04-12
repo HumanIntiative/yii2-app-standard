566f6f3a9ff22d7150e286ecf7550d2424f80aa9
Zein Miftah <zeinmiftah@gmail.com>
2018-04-12 09:41:07 +0700

    Bugfix for deprecated yii\base\Object

--- components/Controller.php
+++ components/Controller.php
@@ -2,8 +2,25 @@
 
 namespace app\components;
 
+use yii\filters\AccessControl;
 use yii\web\Controller as BaseController;
 
 class Controller extends BaseController
 {
+	public function behaviors()
+	{
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+				'only' => ['index'],
+				'rules' => [
+					// allow authenticated users
+					[
+						'allow' => true,
+						'roles' => ['@'],
+					],
+				],
+			],
+		];
+	}
 }
